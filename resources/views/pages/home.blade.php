cd74c9828eefd0c99863a80820b2e596f8ae5df1
Philippe DELEFOSSE <pdelefos@student.42.fr>
2016-11-16 18:53:07 +0100

    register form input much bigger

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.default_noheader', array('url', $url))
 @section('content')
 	<div class='home-wrap'>
-		<a href="#" class="home-connection-link">se connecter</a>
+		<a href="{{ $url }}/connection" class="home-connection-link">se connecter</a>
 		<div class="home-container">
 			<div class="home-pres">
 				<h2 class="home-pres__title">Matcha</h2>
 				<div class="home-pres__info">
-					Site de rencontre cool.
+					Site de rencontre à la cool.
 				</div>
 			</div>
 			<div class="home-register">
-				<h2 class="home-register__title">Inscription</h2>
+				<h2 class="home-register__title">INSCRIPTION</h2>
 				<form action="#" method="post" class="form-register">
 				<input class="form-register__input" type="text" name="email" placeholder="EMAIL">
 				<input class="form-register__input" type="text" name="login" placeholder="PSEUDO">
