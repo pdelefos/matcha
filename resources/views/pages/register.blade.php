55461c81ce7ad0a69de3c132e6fc8a0289ae10f7
Philippe DELEFOSSE <pdelefos@student.42.fr>
2016-11-26 19:47:22 +0100

    home rename in register

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,20 +1,75 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<?= $url ?>
-	<form action="#" method="post">
-		<label>login</label>
-		<input type="text" name="login"><br/>
-		<label>email</label>
-		<input type="text" name="email"><br/>
-		<label>nom</label>
-		<input type="text" name="nom"><br/>
-		<label>prenom</label>
-		<input type="text" name="prenom"><br/>
-		<input type="submit" value="register">
-	</form>
-</body>
-</html>
+<?php
+$page_need = array(
+	'url', $url
+);
+if (isset($errorHandler)) {
+	$error_nom = $errorHandler->first('nom');
+	$error_prenom = $errorHandler->first('prenom');
+	$error_email = $errorHandler->first('email');
+	$error_login = $errorHandler->first('login');
+	$error_password = $errorHandler->first('password');
+}
+if (isset($prev_values)) {
+	$prev_nom = $prev_values->input('nom');
+	$prev_prenom = $prev_values->input('prenom');
+	$prev_email = $prev_values->input('email');
+	$prev_login = $prev_values->input('login');
+}
+?>
+@extends('layouts.default_noheader', $page_need)
+@section('content')
+	<div class='home-wrap'>
+		<a href="{{ $url }}/connection" class="home-connection-link btn-connection btn-hover">se connecter</a>
+		<div class="home-container">
+			<div class="home-pres">
+				<h2 class="home-pres__title">
+					<div class="wavetext"></div>
+				</h2>
+				<div class="home-pres__info">
+					Site de rencontre à la cool.
+				</div>
+			</div>
+			<div class="home-register">
+				<h2 class="home-register__title">INSCRIPTION</h2>
+				<form action="#" method="post" class="form-register">
+				@if (isset($error_nom) && $error_nom != "")
+					<div class="home-register__error" data-error="{{ $error_nom }}" >
+				@else
+					<div>
+				@endif
+					<input class="form-register__input" type="text" name="nom" placeholder="NOM" value="{{ $prev_nom or '' }}">
+				</div>
+				@if (isset($error_prenom) && $error_prenom != "")
+					<div class="home-register__error" data-error="{{ $error_prenom }}">
+				@else
+					<div>
+				@endif
+					<input class="form-register__input" type="text" name="prenom" placeholder="PRENOM" value="{{ $prev_prenom or '' }}">
+				</div>
+				@if (isset($error_email) && $error_email != "")
+					<div class="home-register__error" data-error="{{ $error_email }}">
+				@else
+					<div>
+				@endif
+					<input class="form-register__input" type="text" name="email" placeholder="EMAIL" value="{{ $prev_email or '' }}">
+				</div>
+				@if (isset($error_login) && $error_login != "")
+					<div class="home-register__error" data-error="{{ $error_login }}">
+				@else
+					<div>
+				@endif
+					<input class="form-register__input" type="text" name="login" placeholder="PSEUDO" value="{{ $prev_login or '' }}">
+				</div>
+				@if (isset($error_password) && $error_password != "")
+					<div class="home-register__error" data-error="{{ $error_password }}">
+				@else
+					<div>
+				@endif
+					<input class="form-register__input" type="text" name="password" placeholder="MOT DE PASSE">
+				</div>
+				<input class="form-register__input btn-submit btn-hover" type="submit" name="submit" value="Je m'inscris">
+			</form>
+			</div>
+		<div>
+	</div>
+@stop
