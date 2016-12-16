6c2021a3f2a1d737480e88df34658652309f8624
Philippe DELEFOSSE <pdelefos@student.42.fr>
2016-12-16 14:54:14 +0100

    delete tags settings script in profile view

--- public/js/tags-settings.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#myTags").tagit({
-        itemName: 'item',
-        fieldName: 'interets[]',
-        availableTags: ["c++", "java", "php", "javascript", "ruby", "python", "c"]
-    })
-});
