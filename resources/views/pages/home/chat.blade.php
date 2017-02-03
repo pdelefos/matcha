db5c6eccdd46baa82863f1d2881327feaeeaaa80
Philippe DELEFOSSE <pdelefos@student.42.fr>
2017-02-03 12:30:14 +0100

    chat design

--- resources/views/pages/home/chat.blade.php
+++ resources/views/pages/home/chat.blade.php
@@ -6,6 +6,26 @@ $page_need = array(
 @extends('layouts.default')
 @section('content')
     <div class="app-wrap chat-page">
-        
+        <div class="chat-container">
+            <div class="chat-users">
+                <ul class="chat-users__list">
+                    <li class="chat-users__item">sulo</li>
+                    <li class="chat-users__item">silbo</li>
+                    <li class="chat-users__item">poulet</li>
+                    <li class="chat-users__item">grefayce</li>
+                </ul>
+            </div>
+            <div class="chat-conversation">
+                <div class="chat-conversation__window">
+                
+                </div>
+                <div class="chat-conversation__form">
+                    <form action="">
+                        <input type="text" class="chat-conversation__input">
+                        <input type="submit" class="chat-conversation__submit">
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
